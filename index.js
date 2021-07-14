cf1a75732099147527082b4fccc8320b8ff486d6
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-07-14 23:52:44 +0900

    eat scared ghosts

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function powerPelletEaten() {
         //change each of the four ghosts to isScared
         ghosts.forEach(ghost => ghost.isScared = true)
         //use setTimeout to unscare ghosts after 10 seconds   
-        setTimeout(unScareGhosts, 10000)    
+        setTimeout(unScareGhosts, 100000)    
     }
 }
 
@@ -211,6 +211,19 @@ function moveGhost(ghost) {
         if (ghost.isScared) {
             squares[ghost.currentIndex].classList.add('scared-ghost')
         }
+        
+        //if the ghost is current scared AND pacman is on it
+        if (ghost.isScared && squares[ghost.currentIndex].classList.contains('pacman')) {
+            //remove classnames - ghost.className, 'ghost', 'scared-ghost'
+            squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
+            // change ghosts currentIndex back to its startIndex
+            ghost.currentIndex = ghost.startIndex
+            //add a score of 100
+            score +=100
+            //re-add classnames of ghost.className and 'ghost' to the ghosts new postion  
+            squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+        }
+
 
 
     }, ghost.speed )
